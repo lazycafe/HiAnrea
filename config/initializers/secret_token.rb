4d0237e89011705f527f1af0bfea438cf116dd7d
Andrea Yang <firecomet185@gmail.com>
2012-07-08 12:43:50 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HiAndrea::Application.config.secret_token = '9d1d7b6426742d884e58849137b51321152b6ade40408db7d27e23fff16738debb01a3b4b8b1ed065971582a7dc95a9fde5cb7a0f9e5bcc2323144c1833e617b'
